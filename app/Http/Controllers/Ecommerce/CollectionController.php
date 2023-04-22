13c917efff037487d094f3ce8402da69c8a320bb
aung-thu-zaw <aungthuzaw223@gmail.com>
2023-04-22 12:43:52 +0630

    fixed collection products bug

--- app/Http/Controllers/Ecommerce/CollectionController.php
+++ app/Http/Controllers/Ecommerce/CollectionController.php
@@ -13,14 +13,16 @@ class CollectionController extends Controller
 {
     public function index(): Response|ResponseFactory
     {
-        $collections=Collection::with("products:id,collection_id,image")->paginate(20);
+        $collections=Collection::with(["products:id,collection_id,image"])->paginate(20);
+
 
         return inertia("Ecommerce/Collections/Index", compact("collections"));
     }
 
     public function show(Collection $collection): Response|ResponseFactory
     {
-        $products=Product::where("collection_id", $collection->id)->paginate(20);
+        $products=Product::with(["shop","watchlists","cartItems"])->where("collection_id", $collection->id)->paginate(20);
+
 
         return inertia("Ecommerce/Collections/Show", compact("products", "collection"));
     }
