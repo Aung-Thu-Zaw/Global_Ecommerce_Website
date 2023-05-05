981c73753ed03a41aae8e981f00ee1c4bc370108
aung-thu-zaw <aungthuzaw223@gmail.com>
2023-05-05 15:17:57 +0630

    create order placed email template

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,10 @@ require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
 require __DIR__.'/vendor.php';
 
-Route::get('/', [HomeController::class,"index"])->name("home");
+// Route::get('/', [HomeController::class,"index"])->name("home");
+Route::get('/', function(){
+    return view("mails.order-placed");
+});
 
 Route::controller(ProductController::class)
        ->prefix("/products")
