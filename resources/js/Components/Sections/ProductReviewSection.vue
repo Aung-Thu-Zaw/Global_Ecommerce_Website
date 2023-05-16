a70ed363320eb275267da7ab67d0f21038f07fbd
aung-thu-zaw <aungthuzaw223@gmail.com>
2023-05-16 19:51:37 +0630

    dynamic display product rating star

--- resources/js/Components/Sections/ProductReviewSection.vue
+++ resources/js/Components/Sections/ProductReviewSection.vue
@@ -55,6 +55,10 @@ const fiveStarRating = computed(() => {
 });
 </script>
 
+
+
+
+
 <template>
   <div class="text-center mb-5 p-5">
     <h1 class="font-bold text-slate-600 text-xl my-3">Product Reviews</h1>
@@ -63,60 +67,75 @@ const fiveStarRating = computed(() => {
       <div class="flex items-center mb-3">
         <svg
           aria-hidden="true"
-          class="w-5 h-5 text-yellow-400"
+          class="w-5 h-5"
+          :class="{
+            'text-yellow-400': reviewAvg >= 1,
+            'text-gray-300': reviewAvg < 1,
+          }"
           fill="currentColor"
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <title>First star</title>
           <path
             d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
           ></path>
         </svg>
         <svg
           aria-hidden="true"
-          class="w-5 h-5 text-yellow-400"
+          class="w-5 h-5"
+          :class="{
+            'text-yellow-400': reviewAvg >= 2,
+            'text-gray-300': reviewAvg < 2 || reviewAvg >= 3,
+          }"
           fill="currentColor"
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <title>Second star</title>
           <path
             d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
           ></path>
         </svg>
         <svg
           aria-hidden="true"
-          class="w-5 h-5 text-yellow-400"
+          class="w-5 h-5"
+          :class="{
+            'text-yellow-400': reviewAvg >= 3,
+            'text-gray-300': reviewAvg < 3 || reviewAvg >= 4,
+          }"
           fill="currentColor"
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <title>Third star</title>
           <path
             d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
           ></path>
         </svg>
         <svg
           aria-hidden="true"
-          class="w-5 h-5 text-yellow-400"
+          class="w-5 h-5"
+          :class="{
+            'text-yellow-400': reviewAvg >= 4,
+            'text-gray-300': reviewAvg < 4 || reviewAvg >= 5,
+          }"
           fill="currentColor"
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <title>Fourth star</title>
           <path
             d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
           ></path>
         </svg>
         <svg
           aria-hidden="true"
-          class="w-5 h-5 text-gray-300 dark:text-gray-500"
+          class="w-5 h-5"
+          :class="{
+            'text-yellow-400': reviewAvg >= 5,
+            'text-gray-300': reviewAvg < 5,
+          }"
           fill="currentColor"
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <title>Fifth star</title>
           <path
             d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
           ></path>
@@ -258,5 +277,4 @@ const fiveStarRating = computed(() => {
     </p>
   </div>
 </template>
-
 
