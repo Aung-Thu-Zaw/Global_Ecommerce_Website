697dac965bd599f04738699e5e8d98804f9df703
aung-thu-zaw <aungthuzaw223@gmail.com>
2023-05-18 18:25:25 +0630

    add more field for messages table

--- database/migrations/2023_05_17_143817_create_messages_table.php
+++ database/migrations/2023_05_17_143817_create_messages_table.php
@@ -16,7 +16,10 @@ return new class () extends Migration {
             $table->id();
             $table->foreignId("conversation_id")->constrained()->cascadeOnDelete();
             $table->foreignId("user_id")->constrained()->cascadeOnDelete();
-            $table->text("message");
+            $table->enum('type', ['text', 'image', 'video']);
+            $table->text('text_content')->nullable();
+            $table->string('image_path')->nullable();
+            $table->string('video_path')->nullable();
             $table->timestamps();
         });
     }
