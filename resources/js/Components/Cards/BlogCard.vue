ed38ff8e3c1064948f99c0381f5633bee0436067
aung-thu-zaw <aungthuzaw223@gmail.com>
2023-06-05 20:45:24 +0630

    create frontend blog design

---
+++ resources/js/Components/Cards/BlogCard.vue
@@ -0,0 +1,61 @@
+<script setup>
+</script>
+
+<template>
+  <div
+    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+  >
+    <a href="#">
+      <img
+        class="rounded-t-lg"
+        src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+      />
+    </a>
+    <div class="p-5">
+      <div class="flex items-center justify-between mb-3">
+        <span class="font-bold text-slate-400 text-[.7rem]">
+          <i class="fa-solid fa-user mr-2"></i>
+          Aung Thu Zaw
+        </span>
+        <span class="font-bold text-slate-400 text-[.7rem]">
+          <i class="fa-solid fa-clock mr-2"></i>
+          Feb 16, 2023
+        </span>
+      </div>
+      <a href="#">
+        <h5
+          class="mb-2 text-lg font-bold tracking-tight text-gray-700 dark:text-white line-clamp-3"
+        >
+          Noteworthy technology acquisitions 2021
+        </h5>
+      </a>
+      <p class="mb-3 font-normal text-[.8rem] text-gray-500 line-clamp-4">
+        Here are the biggest enterprise technology acquisitions of 2021 so far,
+        in reverse chronological order.
+      </p>
+      <div class="w-full flex items-center justify-end">
+        <a
+          href="#"
+          class="inline-flex items-center text-sm font-medium text-center text-slate-700 hover:text-blue-700 rounded-lg hover:animate-bounce"
+        >
+          Read more
+          <svg
+            aria-hidden="true"
+            class="w-4 h-4 ml-2 -mr-1"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+
--- resources/js/Pages/Ecommerce/Blogs/Index.vue
+++ resources/js/Pages/Ecommerce/Blogs/Index.vue
@@ -2,7 +2,9 @@
 import AppLayout from "@/Layouts/AppLayout.vue";
 import { toast } from "vue3-toastify";
 import "vue3-toastify/dist/index.css";
-import { usePage, Head } from "@inertiajs/vue3";
+import { usePage, Head, Link } from "@inertiajs/vue3";
+import Breadcrumb from "@/Components/Breadcrumbs/HomeBreadcrumb.vue";
+import BlogCard from "@/Components/Cards/BlogCard.vue";
 
 defineProps({});
 
@@ -16,119 +18,135 @@ if (usePage().props.flash.successMessage) {
 <template>
   <AppLayout>
     <Head title="Blogs" />
+
     <div class="min-h-screen bg-gray-50 mt-40 w-full py-6">
-      <div class="w-[1400px] border mx-auto">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam
-        aperiam et deserunt explicabo quae, architecto, obcaecati adipisci quam
-        nulla modi hic sapiente ratione dolor fuga ut itaque nostrum minima?
-        Alias aut tenetur dolorum ullam placeat libero, explicabo cupiditate
-        eius dignissimos dicta odit deserunt error illum provident minima! Quod
-        eveniet labore corrupti, odit earum corporis! Soluta, sapiente molestiae
-        pariatur sequi officia nam ipsam ipsum delectus! Delectus vel porro
-        cumque ad, voluptate fuga veritatis, enim consectetur quo, pariatur in
-        illum ullam voluptatum dignissimos aut minima aspernatur eaque amet
-        cupiditate et tempora? Aspernatur consequuntur sequi possimus sed
-        beatae, provident accusantium non cum architecto ipsum harum optio
-        voluptatem laudantium ab quae illum deleniti minima magnam natus
-        voluptate dolorem quasi in alias. Nobis consequuntur quod omnis
-        assumenda ad maxime, nesciunt doloribus. Deserunt nobis vitae officia
-        consequatur maiores, magni doloremque fugit reiciendis corrupti et
-        blanditiis maxime sunt distinctio nostrum exercitationem fuga deleniti
-        incidunt! Numquam quod odio aliquam dignissimos pariatur ea atque modi
-        quam cupiditate id commodi praesentium, cum autem accusantium dolore non
-        adipisci nulla excepturi cumque possimus. Repudiandae culpa quaerat
-        quasi officiis, natus eum ipsa harum reprehenderit veritatis odio, optio
-        expedita consectetur, maxime alias rem deserunt doloremque commodi? Ex
-        praesentium dolores nam quos fuga maiores dicta itaque! Dolores facere
-        maxime aspernatur consectetur vero laborum vel veritatis iste non ut
-        repellat, impedit blanditiis, vitae iusto possimus quas, consequuntur
-        voluptatem dolor quo sequi error? A expedita dicta debitis temporibus
-        nisi illo laborum officiis numquam consectetur pariatur necessitatibus
-        doloremque dolore eaque inventore adipisci eius repellat architecto aut
-        placeat tempora consequatur, dolorum error. Optio, deserunt! Odit nihil
-        mollitia suscipit tempora officia possimus architecto, alias error
-        deleniti. Laudantium excepturi modi, accusantium voluptate beatae
-        accusamus nobis iste repellendus similique maxime perferendis pariatur
-        natus vel atque hic distinctio explicabo aperiam officia sit possimus
-        sunt obcaecati fuga. Magni dolor, exercitationem similique sapiente
-        officiis tempora, architecto repudiandae maxime, voluptas aspernatur
-        quidem rerum modi accusantium saepe magnam. Placeat reprehenderit, nemo
-        tempore nam obcaecati exercitationem nesciunt temporibus eos
-        necessitatibus ullam hic voluptate ut beatae magnam! Tempora odio iure,
-        minima modi maxime, ipsum fugit saepe blanditiis similique impedit
-        perspiciatis reprehenderit quia commodi debitis sunt illum maiores
-        ducimus ad consequatur voluptatum dolores officiis eum earum!
-        Reprehenderit fugit adipisci impedit quaerat quidem beatae dicta
-        repellat quam suscipit cumque quisquam laboriosam voluptate magnam,
-        libero labore, a atque cupiditate aliquam eos facilis recusandae
-        voluptatum porro. Quo sapiente fugiat dolorum quae tempore, dignissimos
-        voluptate perferendis eveniet nobis doloribus eaque asperiores vero
-        minus explicabo consequatur beatae tempora, magni impedit illo neque
-        recusandae molestias quam! Placeat magnam dignissimos cupiditate illo
-        libero, cumque nemo eius corrupti non ipsa, dolor consequatur vel!
-        Obcaecati, accusantium voluptate minima at natus eum ullam eos excepturi
-        mollitia delectus repudiandae culpa incidunt, veritatis inventore quos
-        exercitationem reprehenderit vero a velit quibusdam. Reprehenderit,
-        natus blanditiis aliquid nobis provident molestias nulla obcaecati esse,
-        nostrum ducimus earum optio, qui illo laboriosam placeat consequuntur.
-        Labore maiores molestiae debitis, totam nobis eos similique voluptas at
-        alias impedit quos, numquam consequatur pariatur delectus repellat
-        consequuntur perferendis odit commodi distinctio cupiditate dolorem.
-        Tenetur voluptatibus eligendi harum eum velit iusto aut quam
-        exercitationem aliquam placeat? Voluptatem quaerat animi ducimus labore
-        nam suscipit? Provident iusto ex laudantium? Eum minus deserunt ipsa
-        optio cupiditate sit eos quo minima rem dolor voluptatibus voluptates id
-        debitis culpa dolores dolore quibusdam, repellendus nulla odio facere
-        labore doloremque expedita dolorem at. Iste ullam quos repudiandae
-        eligendi. Corrupti sequi quae, non repudiandae nesciunt exercitationem
-        vel adipisci unde nemo, voluptas aut porro assumenda minima! Sequi hic
-        praesentium quidem neque amet natus blanditiis molestias dignissimos qui
-        nobis fugiat omnis modi itaque placeat, fuga incidunt dolores quo optio
-        temporibus! Repellendus corporis, praesentium tempore asperiores illo
-        blanditiis quasi at repellat voluptatum, cumque rerum ipsam deleniti
-        alias ea, commodi harum sequi iure perferendis omnis. Excepturi maiores
-        natus voluptate labore voluptates nemo praesentium, recusandae inventore
-        quos, ducimus magnam consequatur iusto enim eligendi, asperiores nostrum
-        quae modi voluptas nihil tenetur! Veniam dolores iusto harum blanditiis
-        dicta ex suscipit assumenda eius fugiat sapiente, consectetur cum nihil
-        quae aliquam at voluptatem animi quos laborum fugit aliquid iste ut
-        ipsum saepe tenetur. Rerum dicta enim assumenda suscipit blanditiis
-        nesciunt beatae doloribus sunt ipsam saepe amet impedit praesentium vero
-        ipsa, aliquid dolorem illo veritatis sed soluta eligendi voluptatem
-        provident, deserunt voluptatibus modi? Nihil tempora, reprehenderit
-        praesentium quo, cum quos at odit minima repellendus impedit voluptatem
-        autem illum possimus blanditiis explicabo quod voluptatum eos esse
-        facere, fugiat deleniti. Praesentium, est nemo? Recusandae, earum
-        molestias. Quos atque dolorum autem quaerat doloribus commodi est
-        recusandae similique iste quia id animi exercitationem, qui assumenda
-        tempore eum vero quidem? Odit earum perspiciatis magni eveniet,
-        dignissimos adipisci fugiat accusantium hic incidunt tempora reiciendis,
-        pariatur velit tenetur explicabo autem voluptatum veniam officia quasi
-        dolor quod. Enim autem aliquid mollitia deleniti officia nam, similique
-        eligendi praesentium laborum quibusdam? Esse perferendis atque sed eaque
-        beatae libero, molestias ipsa totam eligendi culpa, eius sapiente quam
-        aliquam optio minus reiciendis id quisquam in dolorem nihil! Numquam,
-        harum officiis? Officiis illo corrupti quis velit ipsum fugit nostrum
-        eius, fuga optio impedit voluptatum a iure possimus quaerat voluptatem
-        accusamus, quisquam excepturi rerum, veritatis cum ut illum vel. Et
-        autem quos laboriosam, dolorem perferendis, eligendi eum sapiente illum
-        eveniet accusantium nulla itaque maxime quibusdam dolor tempora aliquid
-        laborum temporibus esse? Nemo maiores, deserunt fugiat eligendi unde
-        distinctio obcaecati cum laborum impedit mollitia deleniti dicta cumque!
-        Cum nihil blanditiis error. Laborum quam culpa nulla ea deserunt iusto
-        id! Quisquam debitis sed voluptatem. Ipsam fugiat molestiae quo corrupti
-        doloremque optio obcaecati? Obcaecati, laudantium ipsum quidem quae
-        ratione incidunt aliquam similique et ex? Reprehenderit debitis nisi
-        officia ad modi amet aliquam ipsa voluptatibus dolore corporis sint,
-        minus, distinctio, culpa praesentium numquam harum nobis animi! Optio
-        vitae, officiis quas minima enim natus esse commodi fugiat corrupti,
-        placeat sunt iure dolor autem architecto eligendi. Ipsum harum
-        asperiores voluptatem fugiat minima explicabo eius inventore
-        accusantium, repellendus pariatur aperiam rem, nulla accusamus fugit,
-        sapiente corrupti. Quasi ad earum, est enim beatae iusto temporibus
-        natus repellendus, ducimus, voluptatibus expedita provident maiores
-        laborum. Odio facilis natus inventore corrupti minima error earum, quod
-        veritatis mollitia, maiores omnis voluptatem sunt alias qui soluta?
+      <div class="w-[1500px] mx-auto">
+        <div class="border-b py-3 mb-5">
+          <Breadcrumb>
+            <li aria-current="page">
+              <div class="flex items-center">
+                <svg
+                  aria-hidden="true"
+                  class="w-6 h-6 text-gray-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <span
+                  class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400"
+                  >Blogs</span
+                >
+              </div>
+            </li>
+          </Breadcrumb>
+        </div>
+
+        <div class="flex items-start space-x-3">
+          <div class="w-[300px]">
+            <ul
+              class="h-auto space-y-3 text-center text-md font-bold text-slate-700"
+            >
+              <li
+                class="py-3 rounded-sm bg-gray-100 hover:bg-gray-200 transition-all border border-slate-300 shadow hover:animate-bounce"
+              >
+                <Link href="#">
+                  Beauty And Fashion
+                  <span
+                    class="text-[.8rem] px-2 ml-3 py-1 rounded-full border-2 border-slate-400"
+                  >
+                    3
+                  </span>
+                </Link>
+              </li>
+              <li
+                class="py-3 rounded-sm bg-gray-100 hover:bg-gray-200 transition-all border border-slate-300 shadow hover:animate-bounce"
+              >
+                <Link href="#">
+                  Beauty And Fashion
+                  <span
+                    class="text-[.8rem] px-2 ml-3 py-1 rounded-full border-2 border-slate-400"
+                  >
+                    3
+                  </span>
+                </Link>
+              </li>
+              <li
+                class="py-3 rounded-sm bg-gray-100 hover:bg-gray-200 transition-all border border-slate-300 shadow hover:animate-bounce"
+              >
+                <Link href="#">
+                  Beauty And Fashion
+                  <span
+                    class="text-[.8rem] px-2 ml-3 py-1 rounded-full border-2 border-slate-400"
+                  >
+                    3
+                  </span>
+                </Link>
+              </li>
+              <li
+                class="py-3 rounded-sm bg-gray-100 hover:bg-gray-200 transition-all border border-slate-300 shadow hover:animate-bounce"
+              >
+                <Link href="#">
+                  Beauty And Fashion
+                  <span
+                    class="text-[.8rem] px-2 ml-3 py-1 rounded-full border-2 border-slate-400"
+                  >
+                    3
+                  </span>
+                </Link>
+              </li>
+              <li
+                class="py-3 rounded-sm bg-gray-100 hover:bg-gray-200 transition-all border border-slate-300 shadow hover:animate-bounce"
+              >
+                <Link href="#">
+                  Beauty And Fashion
+                  <span
+                    class="text-[.8rem] px-2 ml-3 py-1 rounded-full border-2 border-slate-400"
+                  >
+                    3
+                  </span>
+                </Link>
+              </li>
+              <li
+                class="py-3 rounded-sm bg-gray-100 hover:bg-gray-200 transition-all border border-slate-300 shadow hover:animate-bounce"
+              >
+                <Link href="#">
+                  Beauty And Fashion
+                  <span
+                    class="text-[.8rem] px-2 ml-3 py-1 rounded-full border-2 border-slate-400"
+                  >
+                    3
+                  </span>
+                </Link>
+              </li>
+              <li
+                class="py-3 rounded-sm bg-gray-100 hover:bg-gray-200 transition-all border border-slate-300 shadow hover:animate-bounce"
+              >
+                <Link href="#">
+                  Beauty And Fashion
+                  <span
+                    class="text-[.8rem] px-2 ml-3 py-1 rounded-full border-2 border-slate-400"
+                  >
+                    3
+                  </span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div class="w-full">
+            <div class="grid grid-cols-4 gap-5">
+              <BlogCard />
+              <BlogCard />
+              <BlogCard />
+              <BlogCard />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </AppLayout>
