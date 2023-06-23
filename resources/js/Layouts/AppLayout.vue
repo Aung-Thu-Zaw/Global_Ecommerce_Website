6df1a4e8ac8c6d64a4192661c2e7ce44221c8b89
aung-thu-zaw <aungthuzaw223@gmail.com>
2023-06-23 20:16:34 +0630

    fixed frontend in suggestion modal

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -3,7 +3,7 @@ import EcommerceFooter from "@/Components/Footers/EcommerceFooter.vue";
 import EcommerceNavbar from "@/Components/Navbars/EcommerceNavbar.vue";
 import CategoryNavbar from "@/Components/Navbars/CategoryNavbar.vue";
 import EcommerceRightSideNavbar from "@/Components/Navbars/EcommerceRightSideNavbar.vue";
-import { onMounted } from "vue";
+import { onMounted, watch } from "vue";
 import { initFlowbite } from "flowbite";
 import { toast } from "vue3-toastify";
 import "vue3-toastify/dist/index.css";
@@ -13,6 +13,15 @@ import { usePage } from "@inertiajs/vue3";
 onMounted(() => {
   initFlowbite();
 });
+
+watch(
+  () => usePage().props.flash.successMessage,
+  () => {
+    toast.success(usePage().props.flash.successMessage, {
+      autoClose: 2000,
+    });
+  }
+);
 
 if (usePage().props.flash.successMessage) {
   toast.success(usePage().props.flash.successMessage, {
