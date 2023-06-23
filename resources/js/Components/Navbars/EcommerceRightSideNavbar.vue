50ff92f39903d7213e7a8a37a5af8a5c705c8c50
aung-thu-zaw <aungthuzaw223@gmail.com>
2023-06-23 09:54:14 +0630

    add report bugs feature frontend

--- resources/js/Components/Navbars/EcommerceRightSideNavbar.vue
+++ resources/js/Components/Navbars/EcommerceRightSideNavbar.vue
@@ -1,5 +1,6 @@
 <script setup>
 import ChatBox from "@/Components/ChatBox.vue";
+import ReportBugModal from "@/Components/Modals/ReportBugModal.vue";
 import { usePage } from "@inertiajs/vue3";
 import { computed, ref } from "vue";
 
@@ -104,11 +105,14 @@ const totalMessages = computed(() => {
         <button
           data-tooltip-target="report-bugs"
           data-tooltip-placement="left"
-          type="button"
+          data-modal-target="report-modal"
+          data-modal-toggle="report-modal"
           class="mb-2 md:mb-0 text-white shadow-lg bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm text-center w-8 h-8"
+          type="button"
         >
           <i class="fa-solid fa-bug"></i>
         </button>
+
         <div
           id="report-bugs"
           role="tooltip"
@@ -147,7 +151,7 @@ const totalMessages = computed(() => {
             <div
               id="facebook"
               role="tooltip"
-              class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+              class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip"
             >
               Share on facebook
               <div class="tooltip-arrow" data-popper-arrow></div>
@@ -166,7 +170,7 @@ const totalMessages = computed(() => {
             <div
               id="twitter"
               role="tooltip"
-              class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+              class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip"
             >
               Share on twitter
               <div class="tooltip-arrow" data-popper-arrow></div>
@@ -186,7 +190,7 @@ const totalMessages = computed(() => {
             <div
               id="linkedin"
               role="tooltip"
-              class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+              class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip"
             >
               Share on LinkedIn
               <div class="tooltip-arrow" data-popper-arrow></div>
@@ -206,7 +210,7 @@ const totalMessages = computed(() => {
             <div
               id="reddit"
               role="tooltip"
-              class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+              class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip"
             >
               Share on reddit
               <div class="tooltip-arrow" data-popper-arrow></div>
@@ -225,7 +229,7 @@ const totalMessages = computed(() => {
             <div
               id="telegram"
               role="tooltip"
-              class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+              class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip"
             >
               Share on telegram
               <div class="tooltip-arrow" data-popper-arrow></div>
@@ -244,7 +248,7 @@ const totalMessages = computed(() => {
             <div
               id="whatsapp"
               role="tooltip"
-              class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+              class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip"
             >
               Share on whatsapp
               <div class="tooltip-arrow" data-popper-arrow></div>
@@ -257,6 +261,42 @@ const totalMessages = computed(() => {
 
   <div v-if="isVisibleChatbox">
     <ChatBox @isVisible="isVisibleChatbox = false" />
+  </div>
+
+  <!-- Report Bug modal -->
+  <div
+    id="report-modal"
+    tabindex="-1"
+    aria-hidden="true"
+    class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+  >
+    <div class="relative w-full max-w-4xl max-h-full">
+      <!-- Modal content -->
+      <div class="relative bg-white rounded-lg shadow">
+        <button
+          type="button"
+          class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+          data-modal-hide="report-modal"
+        >
+          <svg
+            aria-hidden="true"
+            class="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+          <span class="sr-only">Close modal</span>
+        </button>
+
+        <ReportBugModal />
+      </div>
+    </div>
   </div>
 </template>
 
