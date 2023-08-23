663380743348006510b80500f7d2348931554d2a
aung-thu-zaw <aungthuzaw223@gmail.com>
2023-08-24 04:05:26 +0630

    spread component social media shared

---
+++ resources/js/Components/Cards/ShareOnSocialMediaCard.vue
@@ -0,0 +1,137 @@
+<script setup></script>
+
+<template>
+  <div>
+    <button
+      data-popover-target="share"
+      data-popover-placement="left"
+      type="button"
+      class="mb-2 md:mb-0 text-white shadow-lg bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm text-center w-8 h-8"
+    >
+      <i class="fa-solid fa-share-nodes"></i>
+    </button>
+    <div
+      data-popover
+      id="share"
+      role="tooltip"
+      class="absolute invisible inline-block p-2 text-sm font-medium text-dark bg-white rounded-md shadow-lg border opacity-0 tooltip w-[170px] text-left"
+    >
+      <div class="px-3 font-bold py-1 text-blue-600">
+        <a
+          :href="$page.props.socialShares?.facebook"
+          target="_blank"
+          data-tooltip-target="facebook"
+          data-tooltip-placement="top"
+        >
+          <i class="fa-brands fa-facebook mr-3 text-xl"></i>
+          Facebook
+        </a>
+        <div
+          id="facebook"
+          role="tooltip"
+          class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip"
+        >
+          Share on facebook
+          <div class="tooltip-arrow" data-popper-arrow></div>
+        </div>
+      </div>
+      <div class="px-3 font-bold py-1 text-sky-600">
+        <a
+          :href="$page.props.socialShares?.twitter"
+          target="_blank"
+          data-tooltip-target="twitter"
+          data-tooltip-placement="top"
+        >
+          <i class="fa-brands fa-twitter mr-3 text-xl"></i>
+          Twitter
+        </a>
+        <div
+          id="twitter"
+          role="tooltip"
+          class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip"
+        >
+          Share on twitter
+          <div class="tooltip-arrow" data-popper-arrow></div>
+        </div>
+      </div>
+
+      <div class="px-3 font-bold py-1 text-blue-800">
+        <a
+          :href="$page.props.socialShares?.linkedin"
+          target="_blank"
+          data-tooltip-target="linkedin"
+          data-tooltip-placement="top"
+        >
+          <i class="fa-brands fa-linkedin mr-3 text-xl"></i>
+          Linked In
+        </a>
+        <div
+          id="linkedin"
+          role="tooltip"
+          class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip"
+        >
+          Share on LinkedIn
+          <div class="tooltip-arrow" data-popper-arrow></div>
+        </div>
+      </div>
+      <div data-popper-arrow></div>
+      <div class="px-3 font-bold py-1 text-orange-500">
+        <a
+          :href="$page.props.socialShares?.reddit"
+          target="_blank"
+          data-tooltip-target="reddit"
+          data-tooltip-placement="top"
+        >
+          <i class="fa-brands fa-reddit mr-3 text-xl"></i>
+          Reddit
+        </a>
+        <div
+          id="reddit"
+          role="tooltip"
+          class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip"
+        >
+          Share on reddit
+          <div class="tooltip-arrow" data-popper-arrow></div>
+        </div>
+      </div>
+      <div class="px-3 font-bold py-1 text-blue-500">
+        <a
+          :href="$page.props.socialShares?.telegram"
+          target="_blank"
+          data-tooltip-target="telegram"
+          data-tooltip-placement="top"
+        >
+          <i class="fa-brands fa-telegram mr-3 text-xl"></i>
+          Telegram
+        </a>
+        <div
+          id="telegram"
+          role="tooltip"
+          class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip"
+        >
+          Share on telegram
+          <div class="tooltip-arrow" data-popper-arrow></div>
+        </div>
+      </div>
+      <div class="px-3 font-bold py-1 text-emerald-600">
+        <a
+          :href="$page.props.socialShares?.whatsapp"
+          target="_blank"
+          data-tooltip-target="whatsapp"
+          data-tooltip-placement="top"
+        >
+          <i class="fa-brands fa-whatsapp mr-3 text-xl"></i>
+          Whatsapp
+        </a>
+        <div
+          id="whatsapp"
+          role="tooltip"
+          class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip"
+        >
+          Share on whatsapp
+          <div class="tooltip-arrow" data-popper-arrow></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- resources/js/Components/Navbars/EcommerceRightSideNavbar.vue
+++ resources/js/Components/Navbars/EcommerceRightSideNavbar.vue
@@ -3,6 +3,7 @@ import ChatBox from "@/Components/ChatBox.vue";
 import SuggestionModal from "@/Components/Modals/SuggestionModal.vue";
 import FeedbackModal from "@/Components/Modals/FeedbackModal.vue";
 import NotificationDropdownForUser from "@/Components/Dropdowns/NotificationDropdownForUser.vue";
+import ShareOnSocialMediaCard from "@/Components/Cards/ShareOnSocialMediaCard.vue";
 import { usePage } from "@inertiajs/vue3";
 import { computed, ref } from "vue";
 
@@ -39,10 +40,12 @@ const totalMessages = computed(() => {
     class="fixed bottom-0 top-0 right-0 w-auto px-3 rounded-sm shadow-lg bg-gray-50 border-l transition-all"
   >
     <div class="h-full flex flex-col items-center justify-center space-y-5">
+      <!-- Notification -->
       <NotificationDropdownForUser
         v-if="$page.props.auth.user && $page.props.auth.user.role !== 'admin'"
       />
 
+      <!-- Chat box -->
       <div class="relative">
         <button
           @click="isVisibleChatbox = !isVisibleChatbox"
@@ -70,6 +73,7 @@ const totalMessages = computed(() => {
         </div>
       </div>
 
+      <!-- Suggestion -->
       <div>
         <button
           data-tooltip-target="suggestion"
@@ -91,6 +95,8 @@ const totalMessages = computed(() => {
           <div class="tooltip-arrow" data-popper-arrow></div>
         </div>
       </div>
+
+      <!-- Feedback -->
       <div>
         <button
           data-tooltip-target="feedbacks"
@@ -113,139 +119,8 @@ const totalMessages = computed(() => {
         </div>
       </div>
 
-      <div>
-        <button
-          data-popover-target="share"
-          data-popover-placement="left"
-          type="button"
-          class="mb-2 md:mb-0 text-white shadow-lg bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm text-center w-8 h-8"
-        >
-          <i class="fa-solid fa-share-nodes"></i>
-        </button>
-        <div
-          data-popover
-          id="share"
-          role="tooltip"
-          class="absolute invisible inline-block p-2 text-sm font-medium text-dark bg-white rounded-md shadow-lg border opacity-0 tooltip w-[170px] text-left"
-        >
-          <div class="px-3 font-bold py-1 text-blue-600">
-            <a
-              :href="$page.props.socialShares.facebook"
-              target="_blank"
-              data-tooltip-target="facebook"
-              data-tooltip-placement="top"
-            >
-              <i class="fa-brands fa-facebook mr-3 text-xl"></i>
-              Facebook
-            </a>
-            <div
-              id="facebook"
-              role="tooltip"
-              class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip"
-            >
-              Share on facebook
-              <div class="tooltip-arrow" data-popper-arrow></div>
-            </div>
-          </div>
-          <div class="px-3 font-bold py-1 text-sky-600">
-            <a
-              :href="$page.props.socialShares.twitter"
-              target="_blank"
-              data-tooltip-target="twitter"
-              data-tooltip-placement="top"
-            >
-              <i class="fa-brands fa-twitter mr-3 text-xl"></i>
-              Twitter
-            </a>
-            <div
-              id="twitter"
-              role="tooltip"
-              class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip"
-            >
-              Share on twitter
-              <div class="tooltip-arrow" data-popper-arrow></div>
-            </div>
-          </div>
-
-          <div class="px-3 font-bold py-1 text-blue-800">
-            <a
-              :href="$page.props.socialShares.linkedin"
-              target="_blank"
-              data-tooltip-target="linkedin"
-              data-tooltip-placement="top"
-            >
-              <i class="fa-brands fa-linkedin mr-3 text-xl"></i>
-              Linked In
-            </a>
-            <div
-              id="linkedin"
-              role="tooltip"
-              class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip"
-            >
-              Share on LinkedIn
-              <div class="tooltip-arrow" data-popper-arrow></div>
-            </div>
-          </div>
-          <div data-popper-arrow></div>
-          <div class="px-3 font-bold py-1 text-orange-500">
-            <a
-              :href="$page.props.socialShares.reddit"
-              target="_blank"
-              data-tooltip-target="reddit"
-              data-tooltip-placement="top"
-            >
-              <i class="fa-brands fa-reddit mr-3 text-xl"></i>
-              Reddit
-            </a>
-            <div
-              id="reddit"
-              role="tooltip"
-              class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip"
-            >
-              Share on reddit
-              <div class="tooltip-arrow" data-popper-arrow></div>
-            </div>
-          </div>
-          <div class="px-3 font-bold py-1 text-blue-500">
-            <a
-              :href="$page.props.socialShares.telegram"
-              target="_blank"
-              data-tooltip-target="telegram"
-              data-tooltip-placement="top"
-            >
-              <i class="fa-brands fa-telegram mr-3 text-xl"></i>
-              Telegram
-            </a>
-            <div
-              id="telegram"
-              role="tooltip"
-              class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip"
-            >
-              Share on telegram
-              <div class="tooltip-arrow" data-popper-arrow></div>
-            </div>
-          </div>
-          <div class="px-3 font-bold py-1 text-emerald-600">
-            <a
-              :href="$page.props.socialShares.whatsapp"
-              target="_blank"
-              data-tooltip-target="whatsapp"
-              data-tooltip-placement="top"
-            >
-              <i class="fa-brands fa-whatsapp mr-3 text-xl"></i>
-              Whatsapp
-            </a>
-            <div
-              id="whatsapp"
-              role="tooltip"
-              class="absolute border invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-600 rounded-lg shadow-sm opacity-0 tooltip"
-            >
-              Share on whatsapp
-              <div class="tooltip-arrow" data-popper-arrow></div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <!-- Share On Social Media -->
+      <ShareOnSocialMediaCard />
     </div>
   </div>
 
